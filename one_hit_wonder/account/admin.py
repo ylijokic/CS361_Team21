3794165359fcd76a7018a905c68daccc3f4b675d
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-05-07 14:44:39 -0600

    Add models and admin interface

--- one_hit_wonder/account/admin.py
+++ one_hit_wonder/account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Advertisement, Instrument, Location, Musician
+
+admin.site.register(Advertisement)
+admin.site.register(Instrument)
+admin.site.register(Location)
+admin.site.register(Musician)
