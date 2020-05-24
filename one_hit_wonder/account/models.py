9485c3af5a636b9eec1a03f9d0c2585ba1a8ac82
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-05-24 14:28:28 -0600

    Don't require profile picture in forms

--- one_hit_wonder/account/models.py
+++ one_hit_wonder/account/models.py
@@ -7,7 +7,7 @@ class Musician(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.ForeignKey('Location', on_delete=models.PROTECT)
     instruments = models.ManyToManyField('Instrument')
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics', blank=True)
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
