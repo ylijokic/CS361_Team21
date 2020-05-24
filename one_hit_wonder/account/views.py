6e15e6ac3a11481fdd6fa3fdaa76781f50aae168
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-05-24 14:12:21 -0600

    Populate initial form data when updating a profile

--- one_hit_wonder/account/views.py
+++ one_hit_wonder/account/views.py
@@ -87,32 +87,53 @@ def register(request):
 
 
 def update_profile(request):
-    # Check if the profile update form request is a POST request
+    # Check if the profile update form request is a POST request.
     if request.method == 'POST':
+        # Get the data submitted in the three combined forms.
         musician_form = MusicianProfileForm(request.POST)
         location_form = LocationSubform(request.POST)
         instrument_form = InstrumentSubform(request.POST)
-        # Verify profile form has valid input
+        # Verify all forms have valid input.
         if musician_form.is_valid() and location_form.is_valid() and instrument_form.is_valid():
+            # Create the location and instrument entered by the user,
+            # or locate matching instances in the database.
             musician_location, location_created = Location.objects.get_or_create(**location_form.cleaned_data)
             musician_instrument, instrument_created = Instrument.objects.get_or_create(**instrument_form.cleaned_data)
+
             try:
+                # If the user is being updated, find the user's musician data,
+                # then update it with the information from the form.
                 musician = Musician.objects.get(user=request.user)
                 musician.location = musician_location
                 musician.instruments.set([musician_instrument])
                 musician.looking_for_work = musician_form.cleaned_data.get('looking_for_work')
                 musician.image = musician_form.cleaned_data.get('image')
             except ObjectDoesNotExist:
+                # If the user's musician profile wasn't completed yet, create a musician object based off of the forms,
+                # then associate the musician object with the user.
                 musician, musician_created = Musician.objects.get_or_create(**musician_form.cleaned_data, user=request.user, location=musician_location)
             musician.instruments.set([musician_instrument])
             msgs.success(request, f"Your profile has been updated.")
             return redirect('account-profile')
     else:
+        # If the user is trying to update the musician profile before completing it,
+        # redirect the user back to the homepage to display the form in a modal.
         if profile_is_incomplete(request.user):
             return redirect('account-home')
-        musician_form = MusicianProfileForm()
-        location_form = LocationSubform()
-        instrument_form = InstrumentSubform()
+        # If the user has a completed musician profile, get their current information so we can pre-populate the forms.
+        current_location = Musician.objects.get(user=request.user).location
+        current_instrument = Musician.objects.get(user=request.user).instruments.all()[0]
+
+        musician_form = MusicianProfileForm(instance=request.user.musician)
+        location_form = LocationSubform(initial={
+            'city': current_location.city,
+            'state': current_location.state,
+            'zip_code': current_location.zip_code
+        })
+        instrument_form = InstrumentSubform(initial={
+            'name': current_instrument.name,
+            'skill_level': current_instrument.skill_level
+        })
     return render(request, 'account/complete_profile.html', {
         'musician_form': musician_form,
         'location_form': location_form,
