37f9998e66f450df8efd4afc6b6e1895244b536c
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-05-23 21:48:51 -0600

    Add ModelForms for musician, instrument, and location

--- one_hit_wonder/account/forms.py
+++ one_hit_wonder/account/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+
+from .models import Musician, Instrument, Location
 
 
 # Create a new form class that inherits from the Django UserCreationForm
@@ -11,5 +14,34 @@ class UserRegisterForm(UserCreationForm):
     # This saves form data to User model when form.save() method is called
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1',
-                  'password2']
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2',)
+
+
+class MusicianProfileForm(ModelForm):
+    class Meta:
+        model = Musician
+        fields = ('image', 'looking_for_work',)
+        labels = {
+            'looking_for_work': 'I want to join a band',
+            'image': 'Profile Picture',
+        }
+
+
+# class for the Profile Completion form
+class InstrumentSubform(ModelForm):
+    class Meta:
+        model = Instrument
+        fields = '__all__'
+        labels = {
+            'name': 'Primary Instrument',
+        }
+
+
+# class for the Create Ad form and Profile Completion form
+class LocationSubform(ModelForm):
+    class Meta:
+        model = Location
+        fields = '__all__'
+        labels = {
+            'zip_code': 'ZIP Code',
+        }
