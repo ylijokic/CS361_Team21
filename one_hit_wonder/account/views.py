9dc8e194b31061920ae417768f1c05b71a7367be
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-05-23 21:53:03 -0600

    Display and handle profile form, redirecting when profile is incomplete

--- one_hit_wonder/account/views.py
+++ one_hit_wonder/account/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages as msgs
-from .forms import UserRegisterForm
-from .models import Musician
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from .forms import UserRegisterForm, MusicianProfileForm, InstrumentSubform, LocationSubform
+from .models import Musician, Location, Instrument
 
 posts = [
     {
@@ -15,7 +16,6 @@ posts = [
 
 
 def landing(request):
-    print(request)
     return render(request, 'account/landing.html')
 
 
@@ -23,35 +23,49 @@ def landing(request):
 @login_required
 def home(request):
     context = {
-        'posts': posts
+        'posts': posts,
     }
+    if profile_is_incomplete(request.user):
+        context['incomplete'] = True
+        context['musician_form'] = MusicianProfileForm()
+        context['location_form'] = LocationSubform()
+        context['instrument_form'] = InstrumentSubform()
     return render(request, 'account/home.html', context)
 
 
 # Decorator to check if user is logged in before displaying profile
 @login_required
 def profile(request):
-    context = {
-        'title': 'Profile',
-    }
-    return render(request, 'account/profile.html', context)
+    if profile_is_incomplete(request.user):
+        return redirect('account-home')
+
+    return render(request, 'account/profile.html', {'title': 'Profile'})
 
 
 # Decorator to check if user is logged in before displaying profile
 @login_required
 def messages(request):
+    if profile_is_incomplete(request.user):
+        return redirect('account-home')
+
     return render(request, 'account/messages.html', {'title': 'Messages'})
 
 
 # Decorator to check if user is logged in before displaying profile
 @login_required
 def matches(request):
+    if profile_is_incomplete(request.user):
+        return redirect('account-home')
+
     return render(request, 'account/matches.html', {'title': 'Matches'})
 
 
 # Decorator to check if user is logged in before displaying profile
 @login_required
 def create_ad(request):
+    if profile_is_incomplete(request.user):
+        return redirect('account-home')
+
     return render(request, 'account/create_ad.html', {'title': 'Create Ad'})
 
 
@@ -69,3 +83,30 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'account/register.html', {'form': form})
+
+
+def update_profile(request):
+    # Check if the profile update form request is a POST request
+    if request.method == 'POST':
+        musician_form = MusicianProfileForm(request.POST)
+        location_form = LocationSubform(request.POST)
+        instrument_form = InstrumentSubform(request.POST)
+        # Verify profile form has valid input
+        if musician_form.is_valid() and location_form.is_valid() and instrument_form.is_valid():
+            musician_location, location_created = Location.objects.get_or_create(**location_form.cleaned_data)
+            musician_instrument, instrument_created = Instrument.objects.get_or_create(**instrument_form.cleaned_data)
+            musician, musician_created = Musician.objects.get_or_create(**musician_form.cleaned_data, user=request.user, location=musician_location)
+            musician.instruments.add(musician_instrument)
+            msgs.success(request, f"Your profile has been updated! Enjoy One Hit Wonder.")
+            return redirect('account-profile')
+    else:
+        musician_form = MusicianProfileForm()
+        location_form = LocationSubform()
+        instrument_form = InstrumentSubform()
+    return render(request, 'account/complete_profile.html', {'musician_form': musician_form,
+                                                             'location_form': location_form,
+                                                             'instrument_form': instrument_form})
+
+
+def profile_is_incomplete(user):
+    return not hasattr(user, 'musician') or user.musician.looking_for_work is None
