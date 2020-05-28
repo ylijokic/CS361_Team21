8529c5eedf24c5c144a29768ad62b3b08ba5fde0
Stephen Sullivan <pembroke.ss@gmail.com>
2020-05-28 16:07:12 -0700

    Update views.py
    Added send_ad method.

--- one_hit_wonder/account/views.py
+++ one_hit_wonder/account/views.py
@@ -113,6 +113,36 @@ def create_ad(request):
         subform = LocationSubform()
     return render(request, 'account/create_ad.html', {'form': form, 'subform': subform})
 
+def send_ad(request):
+    # retrieve form data from submit button
+    submit_btn = request.POST.get('submit')
+
+    ad_form = CreateAdForm(request.POST)
+    location_form = LocationSubform(request.POST)
+    musician_form = MusicianProfileForm(request.POST)
+
+    if ad_form.is_valid() and location_form.is_valid() and musician_form.is_valid() and instrument_form.is_valid():
+        #get variables needed for appropriate musician to receive ad
+        ad_location = ad_form.cleaned_data.get('location')
+        ad_instrument = ad_form.cleaned_data.get('name')
+        location = musician_form.cleaned_data.get('city')
+        instrument = musician_form.cleaned_data.getlist('instrument')
+        work = musician_form.cleaned_data.get('looking_for_work')
+
+        # if user has selected looking for work, they will get an ad
+        if work == True:
+            # checks if the instruments match and if locations match
+            if ad_instrument == instrument and ad_location == location:
+                context = {
+                    'title': 'Ad',
+                    'instruments': ad_instrument,
+                    'location': ad_location
+                }
+        else:
+            context = {
+                'title': 'No New Ads'
+            }
+    return render(request, 'account/matches.html', context)
 
 def register(request):
     # Check if the registration form request is a POST request
